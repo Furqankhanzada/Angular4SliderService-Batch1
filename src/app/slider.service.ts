0d66a3854d1a682f76e05b8346beee37f7755af3
Muhammad Furqan <furqan.khanzada@gmail.com>
2017-08-05 18:10:04 +0500

    slider completed

--- src/app/slider.service.ts
+++ src/app/slider.service.ts
@@ -4,12 +4,43 @@ import { Injectable } from '@angular/core';
 export class SliderService {
   translateY: string = 'translateY(0px)';
   slides: any[] = [
-    {active: true},
-    {active: false},
-    {active: true}
+    { active: true, color: 'blue' },
+    { active: false, color: 'green' },
+    { active: false, color: 'pink' },
+    { active: false, color: 'yellow' },
+    { active: false, color: 'red' },
+    { active: false, color: 'violet' },
   ]
   constructor() { }
-  next(){
-    this.translateY = 'translateY(-100px)'
+  getPos() {
+    return this.slides.findIndex(slide => slide.active);
   }
+  setTranslateY() {
+    this.translateY = `translateY(-${this.getPos() * 100}px)`
+  }
+  go(slide){
+    let currentSlide = this.slides[this.getPos()]
+    currentSlide.active = false
+    slide.active = true;
+    this.setTranslateY()
+  }
+  next() {
+    let currentSlide = this.slides[this.getPos()]
+    let nextSlide = this.slides[this.getPos() + 1]
+    if (nextSlide) {
+      currentSlide.active = false
+      nextSlide.active = true
+    }
+    this.setTranslateY()
+  }
+  previous() {
+    let currentSlide = this.slides[this.getPos()]
+    let previousSlide = this.slides[this.getPos() - 1]
+    if (previousSlide) {
+      currentSlide.active = false
+      previousSlide.active = true
+    }
+    this.setTranslateY()
+  }
+
 }
